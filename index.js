bf2fb1291e8234da20672f51abc2f1035c4effce
Rishu Anand <rishuanandchaudhary@yahoo.com>
2018-07-21 08:50:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function shout(strings) {
-  return(strings.toUpperCase())
+function shout(string) {
+  return(string.toUpperCase());
 }
 
-function whisper(strings) {
-  return(strings.toLowerCase())
+function whisper(string) {
+  return(string.toLowerCase());
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
 }
