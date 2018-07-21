c200d69e2e28fa5fb156955b33aa21364061d6fc
Rishu Anand <rishuanandchaudhary@yahoo.com>
2018-07-21 10:05:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,22 +16,28 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return "I can't hear you!"
+    return "I can't hear you!";
+  } else if(string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if(string === "I love you, Grandma.") {
+    return "I love you, too.";
+  } else {
+    return "It's bed time";
   }
 }
-// 
+//
 // function sayHiToGrandma(string) {
 //   if (string.toLowerCase() === string) {
 //     return "I can't hear you!";
 //   }
 // }
-// 
+//
 // function sayHiToGrandma(string) {
 //   if (string.toUpperCase() === string) {
 //     return "YES INDEED!";
 //   }
 // }
-// 
+//
 // function sayHiToGrandma(string) {
 //   if (string === "I love you, Grandma.") {
 //     return("I love you, too.");
