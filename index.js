0ec3d9130901ca4cdfbc4c6c0610613adda79529
Rishu Anand <rishuanandchaudhary@yahoo.com>
2018-07-21 09:38:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
 }
 
 function sayHiToGrandma(string) {
